1dfab1f26229eabd57c1db1066a7866d5efdd30f
Wayne <Wayne@qq.com>
2023-06-28 20:39:28 +0800

    ✨feature: 完善

--- src/components/DesignForm/config/crudFormItem.tsx
+++ src/components/DesignForm/config/crudFormItem.tsx
@@ -198,6 +198,7 @@ const crudFormItem: CrudFormItem[] = [
     type: "SelectOptions",
     isHidden: "{{formData.type !== 'Select'}}",
     options: {
+      mode: (formData) => _.get(formData, "options.mode", ""),
       options: _.get(
         _.find(
           basicComponents,
