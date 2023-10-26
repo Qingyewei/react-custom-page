7c7015cfce5125d16a329ebb8b372decb0def0a2
Wayne <Wayne@qq.com>
2023-10-26 10:30:46 +0800

    ✨feature: 补充信息

--- src/components/DesignForm/utils/index.ts
+++ src/components/DesignForm/utils/index.ts
@@ -5,6 +5,7 @@ function safeEval(code: string) {
 }
 
 // 解析函数字符串值
+// evaluateString('formData.a===rootValue.b',{a:2},{b:2})
 export const evaluateString = (string: string, formData = {}, rootValue = {}) =>
   safeEval(`
   const rootValue =${JSON.stringify(rootValue)};
@@ -12,26 +13,30 @@ export const evaluateString = (string: string, formData = {}, rootValue = {}) =>
   return (${string})
   `);
 
-  export const parseExpression = (func: any, formData = {}, parentPath: string) => {
-    const parentData = _.get(formData, parentPath) || {};
-  
-    if (typeof func === 'string') {
-      const funcBody = func.replace(/^{\s*{/g, '').replace(/}\s*}$/g, '');
-  
-      const funcStr = `
+export const parseExpression = (
+  func: any,
+  formData = {},
+  parentPath: string
+) => {
+  const parentData = _.get(formData, parentPath) || {};
+
+  if (typeof func === "string") {
+    const funcBody = func.replace(/^{\s*{/g, "").replace(/}\s*}$/g, "");
+
+    const funcStr = `
         return ${funcBody
           .replace(/formData/g, JSON.stringify(formData))
           .replace(/rootValue/g, JSON.stringify(parentData))}
       `;
-  
-      try {
-        const result = Function(funcStr)();
-        return result;
-      } catch (error) {
-        console.log(error, parentPath);
-        return null; // 如果计算有错误，return null 最合适
-      }
-    } 
-    
-    return func;
+
+    try {
+      const result = Function(funcStr)();
+      return result;
+    } catch (error) {
+      console.log(error, parentPath);
+      return null; // 如果计算有错误，return null 最合适
+    }
   }
+
+  return func;
+};
