a4c180d7fb36a8d136595d7469513d1560b487d0
Wayne <Wayne@qq.com>
2023-11-07 14:25:58 +0800

    ✨feature: 更新

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import externalGlobals from "rollup-plugin-external-globals";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:'./',
   plugins: [
     react(),
     createSvgIconsPlugin({
