c7fa0605476107729f8670c0e80c0511e41313bf
Wayne <Wayne@qq.com>
2023-11-20 11:14:58 +0800

    ✨feature: 性能分析

--- src/components/DesignForm/core/antd/AntdOperatButton.tsx
+++ src/components/DesignForm/core/antd/AntdOperatButton.tsx
@@ -1,17 +1,22 @@
 import { Button, Row, Space } from "antd";
-import { memo } from "react";
+import { Profiler, memo } from "react";
 import PreviewMode from "./PreviewMode";
 import ViewSourceCode from "./ViewSourceCode";
 
 function OperatButton() {
+  function onRender(id, phase, actualDuration, baseDuration, startTime, commitTime){
+    console.log(`Profiler [${id}] - ${phase} - ${actualDuration} ms`);
+  }
   return (
-    <Row justify="end">
-      <Space wrap>
-        {/* 需要优化首屏加载事件 */}
-        <PreviewMode />
-        <ViewSourceCode />
-      </Space>
-    </Row>
+    <Profiler id="operatButton" onRender={onRender}>
+      <Row justify="end">
+        <Space wrap>
+          {/* 需要优化首屏加载事件 */}
+          <PreviewMode />
+          <ViewSourceCode />
+        </Space>
+      </Row>
+    </Profiler>
   );
 }
 
